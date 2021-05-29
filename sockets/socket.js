27e335fa62712c3c3c1720ba93176b37c62357d0
Daryn Galeas <daryn_galeas@yahoo.com>
2021-05-29 17:23:48 -0600

    Seccion 10 a media - Backend

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,11 +1,32 @@
+const { usuarioConectado, usuarioDesconectado } = require('../controllers/socket');
+const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
 
 
 // Mensajes de Sockets
-io.on('connection', client => {
+io.on('connection', (client) => {
     console.log('Cliente conectado');
 
+    const [valido, uid ] = comprobarJWT(client.handshake.headers['x-token']);
+
+    //Verificar autentificacion
+    if (!valido) { return client.disconnect(); }
+
+    //Cliente autenticado
+    usuarioConectado(uid);
+    
+
+
+    client.join(uid);
+
+    client.on('mensaje-personal', (payload) => {
+        console.log(payload);
+
+        io.to(payload.para).emit('mensaje-personal', payload);
+    })
+
     client.on('disconnect', () => {
+        usuarioDesconectado(uid);
         console.log('Cliente desconectado');
     });
 
